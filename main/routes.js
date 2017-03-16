28b86af843927abadf0bd199655d4c43e8000c6d
Carlin Au <carlinau@gmail.com>
2017-03-16 05:42:06 -0400

    Debug message to see if req was received

--- main/routes.js
+++ main/routes.js
@@ -3,7 +3,8 @@ const config = require('../config/config.js');
 
 const init = function RouteHandler(app) {
   app.get('/status', (req, res)=>{
-    res.send('System Status: OK');
+    console.log('Request Received from: %s', req.ip);
+    res.send("System Status: OK");
   });
 }
 module.exports = init;
